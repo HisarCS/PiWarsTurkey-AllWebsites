91877fe563a8f3dfee675ac1892db7913f8fc12b
cem <cersozz@gmail.com>
2018-12-25 12:29:37 +0300

    all done, added JS

--- js/main.js
+++ js/main.js
@@ -24,5 +24,80 @@ function countDown(){
     document.getElementById("secs").innerHTML =sec ;
 
     setTimeout(countDown,1000);
+}
+jQuery(document).ready(function($) {
+    $('a[href="#top"]').click(function(){
+        $('html, body').animate({scrollTop: 0}, 'slow');
+        return false;
+    });
 
+
+    $('.toggle-menu').click(function(){
+        $('.menu-first').toggleClass('show');
+        // $('.menu-first').slideToggle();
+    });
+
+    $('.menu-first li a').click(function(){
+      $('.menu-first').removeClass('show');
+    });
+
+
+    /*-----------------------------------
+     * ONE PAGE SCROLLING
+     *-----------------------------------*/
+    // Select all links with hashes
+    $('a[href*="#"]').not('[href="#"]').not('[href="#0"]').not('[data-toggle="tab"]').on('click', function(event) {
+        console.log("HERE")
+        // On-page links
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            // Figure out element to scroll to
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            // Does a scroll target exist?
+            if (target.length) {
+                // Only prevent default if animation is actually gonna happen
+                event.preventDefault();
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000, function() {
+                    // Callback after animation
+                    // Must change focus!
+                    var $target = $(target);
+                    $target.focus();
+                    if ($target.is(":focus")) { // Checking if the target was focused
+                        return false;
+                    } else {
+                        $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+                        $target.focus(); // Set focus again
+                    };
+                });
+            }
+        }
+    });
+
+    /*-----------------------------------
+     * NAVBAR CLOSE ON CLICK
+     *-----------------------------------*/
+
+    $('.navbar-nav > li:not(.dropdown) > a').on('click', function() {
+        $('.navbar-collapse').collapse('hide');
+    });
+    onScroll();
+    $(document).on("scroll", onScroll);
+});
+function remove_all(){
+    $('#navbarNav a').each(function () {
+        $(this).removeClass("active");
+    });
+}
+function onScroll(event){
+    var scrollPos = $(document).scrollTop();
+    $('#navbarNav a').each(function () {
+        var currLink = $(this);
+        var refElement = $(currLink.attr("href"));
+        if (refElement.position().top <= scrollPos+5 && refElement.position().top + refElement.height() >= scrollPos-5) {
+            remove_all();
+            currLink.addClass("active");
+        }
+    });
 }
